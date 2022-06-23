e1f1e23952c72d0372d77ce276c93b99d259a6cc
stanl <stanl@LAPTOP-B33KO7M1>
2022-06-22 19:55:24 -0600

    He añadido la clase conexion.java (coneccion fallida) He creado la clase insertar.java con los metodos para insertar datos en las tablas de mysql server
    he hecho avances en la interfaz

---
+++ src/reportesv2/Conexion.java
@@ -0,0 +1,82 @@
+package reportesv2;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    private final String url="bkjemzmxcoa1eelwlf6j-mysql.services.clever-cloud.com";
+    private final String login="udv0mxxkekbzv4rz";
+    private final String password="OIEIxuowHTQhM8O3S7Jr";
+    private final String puerto = "3306";
+    private Connection cnx=null;
+    private Statement sttm=null;
+    private ResultSet rst=null;
+    
+    
+    public Conexion(){
+        try
+        {
+            Class.forName("jdb:sqlserver://"+login+":"+puerto+"/"+login);
+            cnx=DriverManager.getConnection(url, login, password);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ERROR:"+ ex.getMessage());
+            System.exit(1);
+        } catch (ClassNotFoundException c){
+            JOptionPane.showMessageDialog(null,"ERROR:"+ c.getMessage());
+            System.exit(1);            
+        }
+    }
+    
+    //Metodo UID
+    public void UID(String sql){
+        try {
+            Class.forName("\"jdb:sqlserver://\"+login+\":\"+puerto+\"/\"+login");
+            cnx=DriverManager.getConnection(url, login, password);
+            sttm=cnx.createStatement();
+            sttm.executeUpdate(sql);
+        } catch (ClassNotFoundException c) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + c.getMessage());
+            System.exit(1);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + ex.getMessage());
+            System.exit(1);            
+        }
+    }
+    
+    //Metodo Consultar
+    public ResultSet getValores(String sql){
+        try {
+            Class.forName("\"jdb:sqlserver://\"+login+\":\"+puerto+\"/\"+login");
+            cnx=DriverManager.getConnection(url, login, password);
+            sttm=cnx.createStatement();
+            sttm.executeQuery(sql);
+        } catch (ClassNotFoundException c) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + c.getMessage());
+            System.exit(1);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + ex.getMessage());
+            System.exit(1);            
+        }
+        return rst;
+    }
+    
+    //Metodo Cerrar
+    public void cerrar(){
+        try {
+            this.cnx.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + ex.getMessage());
+            System.exit(1); 
+        }
+    }
+    
+    //Metodo Connection
+    public Connection getConexion(){
+        return this.cnx;
+    }
+}
