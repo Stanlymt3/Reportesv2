c497f4db92398efc1c4d6ccbbccfb899a0481f8b
stanl <stanl@LAPTOP-B33KO7M1>
2022-06-23 19:52:20 -0600

    correcciones y avances El ingreso de datos a su respectiva tabla con su respectiva categoria ahora funciona correctamente

--- src/reportesv2/InsertarDatos.java
+++ src/reportesv2/InsertarDatos.java
@@ -10,7 +10,7 @@ public class InsertarDatos {
                 + "('"+Numero+"','"+Nombre_del_producto+"','"+Cantidad+"','"+Precio_sin_iva+"','"+Iva+"','"+Precio_con_iva+"','"+Dinero_total+"')");
     }
     public void insertarAbarrote(String Numero, String Nombre_del_producto, String Cantidad, String Precio_sin_iva, String Iva, String Precio_con_iva, String Dinero_total){
-        cn.UID("INSERT INTO abarrotes(Numero, Nombre_del_Producto, Cantidad, Precio_sin_iva, Iva, Precio_con_iva, Dinero_total) VALUES "
+        cn.UID("INSERT INTO Abarrotes(Numero, Nombre_del_Producto, Cantidad, Precio_sin_iva, Iva, Precio_con_iva, Dinero_total) VALUES "
                 + "('"+Numero+"','"+Nombre_del_producto+"','"+Cantidad+"','"+Precio_sin_iva+"','"+Iva+"','"+Precio_con_iva+"','"+Dinero_total+"')");
     }
     
@@ -25,6 +25,18 @@ public class InsertarDatos {
     
     public ResultSet contarRegistrosBebidas() {
         return (cn.getValores("SELECT COUNT(*) FROM bebidas"));
+    }
+    
+    public ResultSet contarRegistrosAbarrotes() {
+        return (cn.getValores("SELECT COUNT(*) FROM Abarrotes"));
+    }
+    
+    public ResultSet contarRegistrosMedicina() {
+        return (cn.getValores("SELECT COUNT(*) FROM Medicina"));
+    }
+    
+    public ResultSet contarRegistrosLibreria() {
+        return (cn.getValores("SELECT COUNT(*) FROM Libreria"));
     }
     
 }
