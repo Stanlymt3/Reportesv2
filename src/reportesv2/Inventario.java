efaa5b60a8bc938c266a7bc10d1bee2f8fbb786f
stanl <stanl@LAPTOP-B33KO7M1>
2022-06-24 21:01:42 -0600

    avances en boton agregar categoria

--- src/reportesv2/Inventario.java
+++ src/reportesv2/Inventario.java
@@ -28,7 +28,7 @@ public class Inventario extends javax.swing.JFrame {
     int contador = 0, datoTabla, numeroDato;
     String contadorS = "", ivaS, precioIvaS, dineroTotalS, dato, codigo, Tabla,nombre;
     boolean error = false, llave = false, buscar = false;
-    ResultSet rs = null, rt = null, rn = null;
+    ResultSet rs = null, rt = null, rn = null, ru=null;
     InsertarDatos insertarDato = new InsertarDatos();
 
     public void calculos() {
@@ -973,55 +973,50 @@ public class Inventario extends javax.swing.JFrame {
     }//GEN-LAST:event_btnAgregarActionPerformed
 
     private void btnAgregarTodoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarTodoActionPerformed
-        if (cmbRegistro.getSelectedIndex() == 1) {//bebidas
-            Tabla = "bebidas";
-            rt = insertarDato.contarRegistrosT(Tabla);
-        } else if (cmbRegistro.getSelectedIndex() == 2) {//abarrotes
-            Tabla = "Abarrotes";
-            rt = insertarDato.contarRegistrosT(Tabla);
-        } else if (cmbRegistro.getSelectedIndex() == 3) {//medicina
-            Tabla = "Medicina";
-            rt = insertarDato.contarRegistrosT(Tabla);
-        } else if (cmbRegistro.getSelectedIndex() == 4) {//libreria
-            Tabla = "Libreria";
-            rt = insertarDato.contarRegistrosT(Tabla);
-        } else if (cmbRegistro.getSelectedIndex() == 0) {
-            JOptionPane.showMessageDialog(null, "seleccione una categoria");
-        }
-        if (rt != null) {
-
-            try {
-
-                for (int i = 0; i <= 8; i++) {
-                    for (int j = 0; j < insertarDato.contarRegistrosT(Tabla).getInt(1); j++) {
-                        if (cuenta == 0) {
-                            cuenta = 1;
-                            cuentaS = cuenta + "";
-                        } else {
-                            cuenta = cuenta + 1;
-                            cuentaS = cuenta + "";
-                        }
-
-                        String[] datos = new String[8];
-                        datos[0] = cuentaS;
-                        datos[1] = cmbRegistro.getItemAt(cmbRegistro.getSelectedIndex());
-                        datos[2] = txtBuscarNombre.getText();
-                        datos[3] = txtBuscarCantidad.getText();
-                        datos[4] = txtBuscarPrecio.getText();
-                        datos[5] = lblIva.getText();
-                        datos[6] = lblPrecio.getText();
-                        datos[7] = lblDinero.getText();
-
-                    }
-
-                }
-
-            } catch (Exception e) {
+        try {
+            if (cmbRegistro.getSelectedIndex() == 1) {//bebidas
+                Tabla = "bebidas";
+                rt = insertarDato.contarRegistrosT(Tabla);
+            } else if (cmbRegistro.getSelectedIndex() == 2) {//abarrotes
+                Tabla = "Abarrotes";
+                rt = insertarDato.contarRegistrosT(Tabla);
+            } else if (cmbRegistro.getSelectedIndex() == 3) {//medicina
+                Tabla = "Medicina";
+                rt = insertarDato.contarRegistrosT(Tabla);
+            } else if (cmbRegistro.getSelectedIndex() == 4) {//libreria
+                Tabla = "Libreria";
+                rt = insertarDato.contarRegistrosT(Tabla);
+            } else if (cmbRegistro.getSelectedIndex() == 0) {
+                JOptionPane.showMessageDialog(null, "seleccione una categoria");
             }
 
+            for (int i = 0; i <= 8; i++) {
+                for (int j = 0; j < insertarDato.contarRegistrosT(Tabla).getInt(1); j++) {
+                    if (cuenta == 0) {
+                        cuenta = 1;
+                        cuentaS = cuenta + "";
+                    } else {
+                        cuenta = cuenta + 1;
+                        cuentaS = cuenta + "";
+                    }
+
+                    ru = insertarDato.buscar(Tabla, cuentaS);
+
+                    String[] datos = new String[8];
+                    datos[0] = cuentaS;
+                    datos[1] = cmbRegistro.getItemAt(cmbRegistro.getSelectedIndex());
+                    datos[2] = this.ru.getString(2);
+                    datos[3] = this.ru.getString(3);
+                    datos[4] = this.ru.getString(4);
+                    datos[5] = this.ru.getString(5);
+                    datos[6] = this.ru.getString(6);
+                    datos[7] = this.ru.getString(7);
+                }
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ERROR DEL SISTEMA " + e);
         }
-
-
     }//GEN-LAST:event_btnAgregarTodoActionPerformed
 
     /**
